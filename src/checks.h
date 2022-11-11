ef12dd4923d85bcb83a699479d4168722c1dcdb4
EiffL <fr.eiffel@gmail.com>
2022-11-11 18:09:02 +0100

    adding first elements

---
+++ src/checks.h
@@ -0,0 +1,12 @@
+#include <cstdio>
+using namespace std;
+
+// Error checking macros
+#define CHECK_CUDECOMP_EXIT(call)                                                                                      \
+  do {                                                                                                                 \
+    cudecompResult_t err = call;                                                                                       \
+    if (CUDECOMP_RESULT_SUCCESS != err) {                                                                              \
+      fprintf(stderr, "%s:%d CUDECOMP error. (error code %d)\n", __FILE__, __LINE__, err);                             \
+      throw exception();                                               \
+    }                                                                                                                  \
+  } while (false)
