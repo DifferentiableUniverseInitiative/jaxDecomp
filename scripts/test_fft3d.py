d797241646392b0a4ac83c7f8f4fd6c322d345dd
Wassim KABALAN <wastondev@gmail.com>
2024-04-27 02:37:22 +0200

    Added FFT tests - All OK

--- scripts/test_fft3d.py
+++ scripts/test_fft3d.py
@@ -8,8 +8,9 @@ import jax
 import jax.numpy as jnp
 import jaxdecomp
 import time
+from jax.experimental import mesh_utils, multihost_utils
+from jax.sharding import Mesh, PartitionSpec as P
 
-jaxdecomp.init()
 #jaxdecomp.config.update('transpose_comm_backend', jaxdecomp.TRANSPOSE_COMM_MPI_P2P_PL)
 pdims = (2, 2)
 global_shape = (1024, 1024, 1024)
@@ -22,10 +23,15 @@ array = jax.random.normal(
     ],
     key=jax.random.PRNGKey(0)) + rank
 
+# Remap to the global array from the local slice
+devices = mesh_utils.create_device_mesh(pdims[::-1])
+mesh = Mesh(devices, axis_names=('z', 'y'))
+global_array = multihost_utils.host_local_array_to_global_array(
+    array, mesh, P('z', 'y'))
 
 @jax.jit
 def do_fft(x):
-  return jaxdecomp.fft.pfft3d(x, pdims=pdims, global_shape=global_shape)
+  return jaxdecomp.fft.pfft3d(global_array)
 
 
 do_fft(array)
@@ -36,7 +42,7 @@ print(rank, 'took', after - before, 's')
 
 # And now, let's do the inverse FFT
 rec_array = jaxdecomp.fft.pifft3d(
-    karray, pdims=pdims, global_shape=global_shape)
+    karray)
 
 if rank == 0:
   # Let's test if things are like we expect
--- tests/test_fft.py
+++ tests/test_fft.py
@@ -1,150 +1,150 @@
 from mpi4py import MPI
-import os
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-# Manually set the GPU to use
-os.environ["CUDA_VISIBLE_DEVICES"] = "%d" % (rank + 1)
-
-from jax.config import config
-
-config.update("jax_enable_x64", True)
-from numpy.testing import assert_allclose
 import jax
+jax.config.update("jax_enable_x64", True)
+from numpy.testing import assert_allclose
 import jax.numpy as jnp
 import jaxdecomp
 import pytest
+from jax.experimental import mesh_utils, multihost_utils
+from jax.sharding import Mesh, PartitionSpec as P
+from functools import partial
+from math import prod
+from jax.experimental.shard_map import shard_map
 
 # Initialize cuDecomp
-jaxdecomp.init()
+jax.distributed.initialize()
 
-pdims = (1, size)
-global_shape = (29 * size, 19 * size, 17 * size
-               )  # These sizes are prime numbers x size of the pmesh
-x = jax.random.normal(
-    shape=[
-        global_shape[0] // pdims[1], global_shape[1] // pdims[0],
-        global_shape[2]
-    ],
-    key=jax.random.PRNGKey(0))
-# Local value of the array
-array = x + rank
-# Global array
-global_array = jnp.concatenate([x + i for i in range(size)], axis=0)
-# Compute global FFT locally, and account for the transpose
-global_karray = jnp.fft.fftn(global_array).transpose([1, 2, 0])
-global_karray_slice = global_karray[rank * global_shape[1] //
-                                    pdims[1]:(rank + 1) * global_shape[1] //
-                                    pdims[1]]
+# Helper function to create a 3D array and remap it to the global array
+def create_spmd_array(global_shape, pdims):
 
+    assert(len(global_shape) == 3)
+    assert(len(pdims) == 2)
+    assert(prod(pdims) == size) , "The product of pdims must be equal to the number of MPI processes"
 
-def test_fft():
+    local_array = jax.random.normal(
+        shape=[
+            global_shape[0] // pdims[1], global_shape[1] // pdims[0],
+            global_shape[2]
+        ],
+        key=jax.random.PRNGKey(rank))
+    # Remap to the global array from the local slice
+    devices = mesh_utils.create_device_mesh(pdims[::-1])
+    mesh = Mesh(devices, axis_names=('z', 'y'))
+    global_array = multihost_utils.host_local_array_to_global_array(
+        local_array, mesh, P('z', 'y'))
+    
+    return global_array, mesh
+
+pencil_1 = (size // 2, size // (size // 2)) # 2x2 for V100 and 4x2 for A100
+pencil_2 = (size // (size // 2), size // 2) # 2x2 for V100 and 2x4 for A100
+
+@pytest.mark.parametrize("pdims", [(1, size), (size, 1) , pencil_1 , pencil_2]) # Test with Slab and Pencil decompositions
+def test_fft(pdims):
+
+  print("*"*80)
+  print(f"Testing with pdims {pdims}")
+
+  global_shape = (29 * size, 19 * size, 17 * size)  # These sizes are prime numbers x size of the pmesh
+  global_array, mesh = create_spmd_array(global_shape, pdims)
 
   # Perform distributed FFT
-  karray = jaxdecomp.fft.pfft3d(array, pdims=pdims, global_shape=global_shape)
-
-  # Perform inverse FFT
-  rec_array = jaxdecomp.fft.pifft3d(
-      karray, pdims=pdims, global_shape=global_shape)
+  with mesh:
+    karray = jaxdecomp.fft.pfft3d(global_array)
+    # Perform inverse FFT
+    rec_array = jaxdecomp.fft.pifft3d(karray)
 
   # Check the forward FFT
-  assert_allclose(global_karray_slice.real, karray.real, atol=1e-10)
-  assert_allclose(global_karray_slice.imag, karray.imag, atol=1e-10)
+  gathered_array = multihost_utils.process_allgather(global_array, tiled=True)
+  gathered_karray = multihost_utils.process_allgather(karray, tiled=True)
+  gathered_rec_array = multihost_utils.process_allgather(rec_array, tiled=True)
+  jax_karray = jnp.fft.fftn(gathered_array).transpose([1, 2, 0])
+  jax_rec_array = jnp.fft.ifftn(jax_karray).transpose([2, 0, 1])
+  # Check reconstructed array
+  assert_allclose(gathered_array.real, gathered_rec_array.real, rtol=1e-7, atol=1e-7)
+  assert_allclose(gathered_array.imag, gathered_rec_array.imag, rtol=1e-7, atol=1e-7)
   # Check the reverse FFT
-  assert_allclose(array, rec_array, rtol=1e-10, atol=1e-10)
+  assert_allclose(gathered_rec_array.real, jax_rec_array.real, rtol=1e-7, atol=1e-7)
+  assert_allclose(gathered_rec_array.imag, jax_rec_array.imag, rtol=1e-7, atol=1e-7)
+
+@pytest.mark.parametrize("pdims", [(1, size), (size, 1) , pencil_1 , pencil_2]) # Test with Slab and Pencil decompositions
+def test_grad(pdims):
 
 
-def test_jit():
-  # Perform distributed FFT
-  karray = jax.jit(lambda x: jaxdecomp.fft.pfft3d(
-      x, pdims=pdims, global_shape=global_shape))(
-          array)
+  print("*"*80)
+  print(f"Testing with pdims {pdims}")
 
-  # Perform inverse FFT
-  rec_array = jax.jit(lambda x: jaxdecomp.fft.pifft3d(
-      x, pdims=pdims, global_shape=global_shape))(
-          karray)
+  global_shape = (29 * size, 19 * size, 17 * size)  # These sizes are prime numbers x size of the pmesh
+  global_array, mesh = create_spmd_array(global_shape, pdims)
+  
+  print("-"*40)
+  print(f"Testing fwd grad")
 
-  # Check the forward FFT
-  assert_allclose(
-      global_karray[rank * global_shape[1] // pdims[1]:(rank + 1) *
-                    global_shape[1] // pdims[1]].real,
-      karray.real,
-      atol=1e-10)
-  assert_allclose(
-      global_karray[rank * global_shape[1] // pdims[1]:(rank + 1) *
-                    global_shape[1] // pdims[1]].imag,
-      karray.imag,
-      atol=1e-10)
-  # Check the reverse FFT
-  assert_allclose(array, rec_array, rtol=1e-10, atol=1e-10)
-
-
-def test_wrong_array_size():
-  # Let's create an array that doesnt have the size of the local slice
-  array = jnp.ones([16, 16, 16])
-
-  with pytest.raises(AssertionError) as excinfo:
-    karray = jaxdecomp.fft.pfft3d(array, pdims=pdims, global_shape=global_shape)
-
-  assert "Only array dimensions divisible" in str(excinfo.value)
-
-
-def test_grad_fwd():
-  from mpi4jax import allreduce
-
-  # Perform distributed FFT
-  def fun(arr):
-    y = jaxdecomp.fft.pfft3d(arr, pdims=pdims, global_shape=global_shape)
+  @jax.jit
+  def spmd_grad(arr):
+    y = jaxdecomp.fft.pfft3d(arr)
     y = (y * jnp.conjugate(y)).real.sum()
-    return allreduce(y, op=MPI.SUM)[0].sum()
+    return y
 
-  array_grad = jax.grad(fun)(array)
-  print("Here is the gradient I'm getting", array_grad.shape)
-
-  # Perform local FFT
-  @jax.grad
-  def ref_fun(arr):
+    # Perform local FFT
+  @jax.jit
+  def local_grad(arr):
     y = jnp.fft.fftn(arr).transpose([1, 2, 0])
-    return (y * jnp.conjugate(y)).real.sum()
+    y =  (y * jnp.conjugate(y)).real.sum()
+    return y
 
-  ref_grad = ref_fun(global_array)
-  ref_grad = ref_grad[rank * global_shape[0] // pdims[1]:(rank + 1) *
-                      global_shape[0] // pdims[1]]
+  with mesh:
+    # Perform distributed FFT
+    array_grad = jax.grad(spmd_grad)(global_array)
+    print("Here is the gradient I'm getting", array_grad.shape)
 
+
+  gathered_array = multihost_utils.process_allgather(global_array, tiled=True)
+  gathered_grads = multihost_utils.process_allgather(array_grad, tiled=True)
+  jax_grad = jax.grad(local_grad)(gathered_array)
+
+  print(f"Shape of JAX array {jax_grad.shape}")
   # Check the gradients
-  assert_allclose(ref_grad, array_grad, rtol=1e-10, atol=1e-10)
+  assert_allclose(jax_grad, gathered_grads, rtol=1e-5, atol=1e-5)
 
+  print("-"*40)
+  print(f"Testing backward grad")
 
-def test_grad_bwd():
-  from mpi4jax import allreduce
-
-  # Perform distributed FFT
-  def fun(arr):
-    y = jaxdecomp.fft.pifft3d(arr, pdims=pdims, global_shape=global_shape)
+  @jax.jit
+  def inv_spmd_grad(arr):
+    y = jaxdecomp.fft.pifft3d(arr)
     y = (y * jnp.conjugate(y)).real.sum()
-    return allreduce(y, op=MPI.SUM)[0].sum()
+    return y
 
-  array_grad = jax.grad(fun)(global_karray_slice)
-  print("Here is the gradient I'm getting", array_grad.shape)
-
-  # Perform local FFT
-  @jax.grad
-  def ref_fun(arr):
+  @jax.jit
+  def inv_local_grad(arr):
     y = jnp.fft.ifftn(arr).transpose([2, 0, 1])
     return (y * jnp.conjugate(y)).real.sum()
+  
+  with mesh:
+    # Perform distributed FFT
+    ifft_array_grad = jax.grad(inv_spmd_grad)(global_array)
+    print("Here is the gradient I'm getting", array_grad.shape)
+  
+  ifft_gathered_grads = multihost_utils.process_allgather(ifft_array_grad, tiled=True)
+  ifft_jax_grad = jax.grad(inv_local_grad)(gathered_array)
 
-  ref_grad = ref_fun(global_karray)
-  ref_grad = ref_grad[rank * global_shape[0] // pdims[1]:(rank + 1) *
-                      global_shape[0] // pdims[1]]
+  print(f"Shape of JAX array {ifft_jax_grad.shape}")
 
   # Check the gradients
-  assert_allclose(ref_grad, array_grad, rtol=1e-10, atol=1e-10)
+  assert_allclose(ifft_jax_grad, ifft_gathered_grads, rtol=1e-5, atol=1e-5)
 
+   
 
 @pytest.mark.skip(reason="vmap is not yet implemented for the 3D FFT")
 def test_vmap():
+
+  global_shape = (29 * size, 19 * size, 17 * size)  # These sizes are prime numbers x size of the pmesh
+  pdims = (1, size)
+
   x = jax.random.normal(
       shape=[
           128, global_shape[0] // pdims[1], global_shape[1] // pdims[0],
@@ -181,3 +181,12 @@ def test_vmap():
       atol=1e-10)
   # Check the reverse FFT
   assert_allclose(array, rec_array, rtol=1e-10, atol=1e-10)
+
+# find a way to finalize pytest
+def test_end():
+  # Make sure that it is cleaned up
+  # This has to be this way because pytest runs the "global code" before running the tests
+  # There are other solutions https://stackoverflow.com/questions/41871278/pytest-run-a-function-at-the-end-of-the-tests
+  # but this require the least amount of work
+  jaxdecomp.finalize()
+  jax.distributed.shutdown()
