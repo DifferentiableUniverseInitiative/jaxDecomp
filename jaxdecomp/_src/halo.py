6ccdfc97e67aeb6a66a643df539cf1624160dcfe
Wassim KABALAN <wastondev@gmail.com>
2024-06-19 17:06:24 +0200

    Clean-up Halo

--- jaxdecomp/_src/halo.py
+++ jaxdecomp/_src/halo.py
@@ -14,100 +14,127 @@ from jaxlib.hlo_helpers import custom_call
 
 import jaxdecomp
 from jaxdecomp._src import _jaxdecomp
+from jaxdecomp._src.spmd_ops import (BasePrimitive, get_axis_size,
+                                     register_primitive)
 
 
-# This is the inner primitive and it should not be used directly
-def inner_halo_exchange(x,
-                        *,
-                        halo_extents: Tuple[int, int, int],
-                        halo_periods: Tuple[bool, bool, bool],
-                        reduce_halo: bool = True,
-                        pdims: Tuple[int, int] = (1, 1),
-                        global_shape: Tuple[int, int, int] = [1024, 1024,
-                                                              1024]):
-  # TODO: check float or real
-  return inner_halo_p.bind(
-      x,
-      halo_extents=halo_extents,
-      halo_periods=halo_periods,
-      reduce_halo=reduce_halo,
-      pdims=pdims,
-      global_shape=global_shape)
+class HaloPrimitive(BasePrimitive):
 
+  name = "halo_exchange"
+  multiple_results = False
+  impl_static_args = (1, 2, 3)
+  inner_primitive = None
 
-def inner_halo_abstract_eval(x, halo_extents, halo_periods, reduce_halo, pdims,
-                             global_shape):
-  # The return shape is equal to the global shape for the inner primitive (the one that is not exposed)
-  # The return shape is equal to the slice shape for the outer primitive (the one that is exposed)
-  # in all cases it is x.shape
-  return x.update(shape=x.shape, dtype=x.dtype)
+  @staticmethod
+  def abstract(x, halo_extents, halo_periods, reduce_halo, pdims, global_shape):
+    return x.update(shape=x.shape, dtype=x.dtype)
 
+  @staticmethod
+  def outer_abstract(x, halo_extents, halo_periods, reduce_halo, pdims,
+                     global_shape):
+    return x.update(shape=x.shape, dtype=x.dtype)
 
-def inner_halo_lowering(ctx, x, *, halo_extents, halo_periods, reduce_halo,
-                        pdims, global_shape):
-  (x_aval,) = ctx.avals_in
-  x_type = ir.RankedTensorType(x.type)
-  n = len(x_type.shape)
+  @staticmethod
+  def lowering(ctx, x, halo_extents, halo_periods, reduce_halo, pdims,
+               global_shape):
+    (x_aval,) = ctx.avals_in
+    x_type = ir.RankedTensorType(x.type)
+    n = len(x_type.shape)
 
-  is_double = np.finfo(x_aval.dtype).dtype == np.float64
+    is_double = np.finfo(x_aval.dtype).dtype == np.float64
 
-  # Compute the descriptor for our FFT
-  config = _jaxdecomp.GridConfig()
-  config.pdims = pdims
-  config.gdims = global_shape[::-1]
-  config.halo_comm_backend = jaxdecomp.config.halo_comm_backend
-  config.transpose_comm_backend = jaxdecomp.config.transpose_comm_backend
+    # Compute the descriptor for our FFT
+    config = _jaxdecomp.GridConfig()
+    config.pdims = pdims
+    config.gdims = global_shape[::-1]
+    config.halo_comm_backend = jaxdecomp.config.halo_comm_backend
+    config.transpose_comm_backend = jaxdecomp.config.transpose_comm_backend
 
-  workspace_size, opaque = _jaxdecomp.build_halo_descriptor(
-      config, is_double, halo_extents[::-1], halo_periods[::-1], 0)
-  layout = tuple(range(n - 1, -1, -1))
+    workspace_size, opaque = _jaxdecomp.build_halo_descriptor(
+        config, is_double, halo_extents[::-1], halo_periods[::-1], 0)
+    layout = tuple(range(n - 1, -1, -1))
 
-  workspace = mlir.full_like_aval(
-      ctx, 0, jax.core.ShapedArray(shape=[workspace_size], dtype=np.byte))
+    workspace = mlir.full_like_aval(
+        ctx, 0, jax.core.ShapedArray(shape=[workspace_size], dtype=np.byte))
 
-  # reduce_halo is not used in the inner primitive
-  out = custom_call(
-      "halo",
-      result_types=[x_type],
-      operands=[x, workspace],
-      operand_layouts=[layout, (0,)],
-      result_layouts=[layout],
-      has_side_effect=True,
-      operand_output_aliases={0: 0},
-      backend_config=opaque,
-  )
-  return out.results
+    # reduce_halo is not used in the inner primitive
+    out = custom_call(
+        "halo",
+        result_types=[x_type],
+        operands=[x, workspace],
+        operand_layouts=[layout, (0,)],
+        result_layouts=[layout],
+        has_side_effect=True,
+        operand_output_aliases={0: 0},
+        backend_config=opaque,
+    )
+    return out.results
 
+  @staticmethod
+  def impl(x, halo_extents, halo_periods, reduce_halo):
 
-def inner_halo_transpose_rule(x, operand, halo_extents, halo_periods, pdims,
-                              global_shape):
-  result = halo_exchange(x, halo_extents, halo_periods, pdims, global_shape)
-  return (result,)
+    pdims = (1, jax.device_count())
+    global_shape = x.shape
 
+    return HaloPrimitive.inner_primitive.bind(
+        x,
+        halo_extents=halo_extents,
+        halo_periods=halo_periods,
+        reduce_halo=reduce_halo,
+        pdims=pdims,
+        global_shape=global_shape,
+    )
 
-# Custom Primitive
+  @staticmethod
+  def per_shard_impl(x, halo_extents, halo_periods, reduce_halo, pdims,
+                     global_shape):
+    output = HaloPrimitive.inner_primitive.bind(
+        x,
+        halo_extents=halo_extents,
+        halo_periods=halo_periods,
+        reduce_halo=reduce_halo,
+        pdims=pdims,
+        global_shape=global_shape,
+    )
 
+    if reduce_halo:
 
-def get_axis_size(sharding, index):
-  axis_name = sharding.spec[index]
-  if axis_name == None:
-    return 1
-  else:
-    return sharding.mesh.shape[sharding.spec[index]]
+      halo_x, halo_y, halo_z = halo_extents
 
+      ## Apply correction along x
+      if halo_x > 0:
+        output = output.at[halo_x:halo_x + halo_x // 2].add(output[:halo_x //
+                                                                   2])
+        output = output.at[-(halo_x + halo_x // 2):-halo_x].add(
+            output[-halo_x // 2:])
+      ## Apply correction along y
+      if halo_y > 0:
+        output = output.at[:, halo_y:halo_y + halo_y // 2].add(
+            output[:, :halo_y // 2])
+        output = output.at[:, -(halo_y + halo_y // 2):-halo_y].add(
+            output[:, -halo_y // 2:])
+      ## Apply correction along z
+      if halo_z > 0:
+        output = output.at[:, :, halo_z:halo_z + halo_z // 2].add(
+            output[:, :, :halo_z // 2])
+        output = output.at[:, :, -(halo_z + halo_z // 2):-halo_z].add(
+            output[:, :, -halo_z // 2:])
 
-def partition(halo_extents, halo_periods, reduce_halo, mesh, arg_shapes,
-              result_shape):
+    return output
 
-  # halo_exchange has three operands
-  # x, halo_extents, halo_periods
-  # (sanity check)
-  #assert len(arg_shapes) == 3 , "halo_exchange must have only 3 operands in the partitioning lower function"
-  # only x is sharded the other are fully replicated
-  halo_exchange_sharding = arg_shapes[0].sharding
+  @staticmethod
+  def infer_sharding_from_operands(halo_extents, halo_periods, reduce_halo,
+                                   mesh, arg_shapes, result_shape):
+    # Sharding is the same here aswell because halo_exchange is a pointwise operation
+    halo_exchange_sharding = arg_shapes[0].sharding
+    return NamedSharding(mesh, P(*halo_exchange_sharding.spec))
 
-  def lower_fn(operand):
+  @staticmethod
+  def partition(halo_extents, halo_periods, reduce_halo, mesh, arg_shapes,
+                result_shape):
 
+    halo_exchange_sharding = NamedSharding(mesh,
+                                           P(*arg_shapes[0].sharding))
     global_shape = arg_shapes[0].shape
     pdims = (get_axis_size(halo_exchange_sharding,
                            1), get_axis_size(halo_exchange_sharding, 0))
@@ -116,58 +143,31 @@ def partition(halo_extents, halo_periods, reduce_halo, mesh, arg_shapes,
                           global_shape[1] - 2 * pdims[0] * halo_extents[1],\
                           global_shape[2] - 2 * halo_extents[2])
 
-    output = inner_halo_exchange(operand, halo_extents=halo_extents, \
-      halo_periods=halo_periods, pdims=pdims, global_shape=shape_without_halo)
+    impl = partial(
+        HaloPrimitive.per_shard_impl,
+        halo_extents=halo_extents,
+        halo_periods=halo_periods,
+        reduce_halo=reduce_halo,
+        pdims=pdims,
+        global_shape=shape_without_halo)
 
-    if reduce_halo:
-      ## Apply correction along x
-      output = output.at[halo_extents[0]:2 * halo_extents[0]].add(
-          output[:halo_extents[0]])
-      output = output.at[-2 * halo_extents[0]:-halo_extents[0]].add(
-          output[-halo_extents[0]:])
-      ## Apply correction along y
-      output = output.at[:, halo_extents[1]:2 * halo_extents[1]].add(
-          output[:, :halo_extents[1]])
-      output = output.at[:, -2 * halo_extents[1]:-halo_extents[1]].add(
-          output[:, -halo_extents[1]:])
-
-    return output
-
-  return mesh, lower_fn,  \
-      result_shape.sharding, \
-      (halo_exchange_sharding,)
+    return mesh, impl, halo_exchange_sharding, (halo_exchange_sharding,)
 
 
-def infer_sharding_from_operands(halo_extents, halo_periods, reduce_halo, mesh,
-                                 arg_shapes, result_shape):
-  # Sharding is the same here aswell because halo_exchange is a pointwise operation
-  halo_exchange_sharding = arg_shapes[0].sharding
-  return halo_exchange_sharding
+register_primitive(HaloPrimitive)
 
 
-@partial(custom_partitioning, static_argnums=(1, 2, 3))
 def halo_p_lower(x, halo_extents, halo_periods, reduce_halo):
 
-  size = jax.device_count()
-  # The pdims product must be equal to the number of devices because this is checked both in the abstract eval and in cudecomp
-  dummy_pdims = (1, size)
-  dummy_global = x.shape
-  return inner_halo_exchange(x, halo_extents=halo_extents, halo_periods=halo_periods,reduce_halo = reduce_halo,\
-     pdims=dummy_pdims, global_shape=dummy_global)
+  return HaloPrimitive.outer_primitive.bind(
+      x,
+      halo_extents=halo_extents,
+      halo_periods=halo_periods,
+      reduce_halo=reduce_halo,
+  )
 
 
 # declare primitive
-
-inner_halo_p = Primitive("halo_exchange")
-inner_halo_p.def_impl(partial(xla.apply_primitive, inner_halo_p))
-inner_halo_p.def_abstract_eval(inner_halo_abstract_eval)
-ad.deflinear2(inner_halo_p, inner_halo_transpose_rule)
-mlir.register_lowering(inner_halo_p, inner_halo_lowering, platform="gpu")
-
-# Define the partitioning for the primitive
-halo_p_lower.def_partition(
-    partition=partition,
-    infer_sharding_from_operands=infer_sharding_from_operands)
 
 
 # Custom Partitioning
