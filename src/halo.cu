0ae6f5674c5811a95279ec60b13b0405c31167e3
Wassim KABALAN <wastondev@gmail.com>
2024-07-05 12:41:27 +0200

    Allocate using cuda for halo_exchange by default ..allow fallback to XLA

--- src/halo.cu
+++ src/halo.cu
@@ -24,16 +24,12 @@ HRESULT HaloExchange<real_t>::get_halo_descriptor(cudecompHandle_t handle, size_
   CHECK_CUDECOMP_EXIT(
       cudecompGetPencilInfo(handle, m_GridConfig, &m_PencilInfo, halo_desc.axis, halo_desc.halo_extents.data()));
 
-  cudecompPencilInfo_t no_halo;
-
-  // Get pencil information for the specified axis
-  CHECK_CUDECOMP_EXIT(cudecompGetPencilInfo(handle, m_GridConfig, &no_halo, halo_desc.axis, nullptr));
-
   // Get workspace size
   int64_t workspace_num_elements;
   CHECK_CUDECOMP_EXIT(cudecompGetHaloWorkspaceSize(handle, m_GridConfig, halo_desc.axis, m_PencilInfo.halo_extents,
                                                    &workspace_num_elements));
 
+  // TODO(Wassim) Handle complex numbers
   int64_t dtype_size;
   if (halo_desc.double_precision)
     CHECK_CUDECOMP_EXIT(cudecompGetDataTypeSize(CUDECOMP_DOUBLE, &dtype_size));
@@ -41,6 +37,20 @@ HRESULT HaloExchange<real_t>::get_halo_descriptor(cudecompHandle_t handle, size_
     CHECK_CUDECOMP_EXIT(cudecompGetDataTypeSize(CUDECOMP_FLOAT, &dtype_size));
 
   work_size = dtype_size * workspace_num_elements;
+
+  static const char* cudalloc = std::getenv("JD_ALLOCATE_WITH_XLA");
+
+  if (cudalloc = nullptr) {
+
+    CHECK_CUDECOMP_EXIT(cudecompMalloc(handle, m_GridConfig, reinterpret_cast<void**>(&m_WorkSizeBuffer),
+                                       workspace_num_elements * dtype_size));
+
+    StartTraceInfo(m_Tracer) << "cudaMalloc will Allocate for Halo_exchange" << std::endl;
+
+  } else {
+    m_WorkSizeBuffer = nullptr;
+    StartTraceInfo(m_Tracer) << "XLA will Allocate for Halo_exchange" << std::endl;
+  }
 
   return S_OK;
 }
@@ -51,27 +61,45 @@ HRESULT HaloExchange<real_t>::halo_exchange(cudecompHandle_t handle, haloDescrip
   void* data_d = buffers[0];
   void* work_d = buffers[1];
 
-  // desc.axis = 2;
+  void* buffer_to_user = nullptr;
+  if (m_WorkSizeBuffer != nullptr) {
+    // CUDA allocate buffer and managed by me
+    buffer_to_user = m_WorkSizeBuffer;
+  } else {
+    // XLA allocate buffer and managed by XLA
+    buffer_to_user = work_d;
+  }
+
   //  Perform halo exchange along the three dimensions
   for (int i = 0; i < 3; ++i) {
     switch (desc.axis) {
     case 0:
-      CHECK_CUDECOMP_EXIT(cudecompUpdateHalosX(handle, m_GridConfig, data_d, work_d, get_cudecomp_datatype(real_t(0)),
-                                               m_PencilInfo.halo_extents, desc.halo_periods.data(), i, stream));
+      CHECK_CUDECOMP_EXIT(cudecompUpdateHalosX(handle, m_GridConfig, data_d, buffer_to_user,
+                                               get_cudecomp_datatype(real_t(0)), m_PencilInfo.halo_extents,
+                                               desc.halo_periods.data(), i, stream));
       break;
     case 1:
-      CHECK_CUDECOMP_EXIT(cudecompUpdateHalosY(handle, m_GridConfig, data_d, work_d, get_cudecomp_datatype(real_t(0)),
-                                               m_PencilInfo.halo_extents, desc.halo_periods.data(), i, stream));
+      CHECK_CUDECOMP_EXIT(cudecompUpdateHalosY(handle, m_GridConfig, data_d, buffer_to_user,
+                                               get_cudecomp_datatype(real_t(0)), m_PencilInfo.halo_extents,
+                                               desc.halo_periods.data(), i, stream));
       break;
     case 2:
-      CHECK_CUDECOMP_EXIT(cudecompUpdateHalosZ(handle, m_GridConfig, data_d, work_d, get_cudecomp_datatype(real_t(0)),
-                                               m_PencilInfo.halo_extents, desc.halo_periods.data(), i, stream));
+      CHECK_CUDECOMP_EXIT(cudecompUpdateHalosZ(handle, m_GridConfig, data_d, buffer_to_user,
+                                               get_cudecomp_datatype(real_t(0)), m_PencilInfo.halo_extents,
+                                               desc.halo_periods.data(), i, stream));
       break;
     }
   }
 
   return S_OK;
 };
+
+template <typename real_t> HRESULT HaloExchange<real_t>::cleanUp(cudecompHandle_t handle) {
+  // Destroy the memory buffer allocate in case of cudaMalloc
+  // In case of XLA allocation, this buffer is nullptr
+  if (m_WorkSizeBuffer != nullptr) { CHECK_CUDECOMP_EXIT(cudecompFree(handle, m_GridConfig, m_WorkSizeBuffer)); }
+  return S_OK;
+}
 
 template class HaloExchange<float>;
 template class HaloExchange<double>;
