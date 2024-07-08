e465af35de80800f36b77571dd2a49840153adad
EiffL <fr.eiffel@gmail.com>
2024-07-08 02:04:57 -0400

    updating implementation

--- examples/lpt_nbody_demo.py
+++ examples/lpt_nbody_demo.py
@@ -1,40 +1,39 @@
-import jax
-import jaxdecomp
-
-jax.distributed.initialize()
-rank = jax.process_index()
-size = jax.process_count()
-
-print(f"Started process {rank} of {size}")
-
 import argparse
 import os
-import time
 from functools import partial
+from typing import Tuple
 
-import jax.lax as lax
+import jax
+
+jax.config.update('jax_enable_x64', False)
+
 import jax.numpy as jnp
 import jax_cosmo as jc
 import numpy as np
-from growth import dGfa, growth_factor, growth_rate
 from jax.experimental import mesh_utils
 from jax.experimental.shard_map import shard_map
 from jax.sharding import Mesh, NamedSharding
 from jax.sharding import PartitionSpec as P
+from scatter import scatter
+
+import jaxdecomp
+
 
 def _global_to_local_size(mesh_shape, sharding):
-    """ Utility function to compute the expected local size of a mesh
-    given the global size and the sharding strategy. 
-    """
-    return mesh_shape # TODO: sort out how to get the information from sharding
+  """ Utility function to compute the expected local size of a mesh
+      given the global size and the sharding strategy.
+  """
+  pdims = sharding.mesh.devices.shape
+  return [mesh_shape[0] // pdims[1], mesh_shape[1] // pdims[0], mesh_shape[2]]
 
-def fttk(mesh_shape: Tuple[int, int, int], sharding) -> list:
-    """
+
+def fttk(nc: int, sharding) -> list:
+  """
     Generate Fourier transform wave numbers for a given mesh.
 
     Parameters
     ----------
-    mesh_shape : tuple of int
+    mesh_shape : int
         Shape of the mesh grid.
     sharding : Any
         Sharding strategy for the array.
@@ -44,230 +43,273 @@ def fttk(mesh_shape: Tuple[int, int, int], sharding) -> list:
     list
         List of wave number arrays for each dimension.
     """
-    kd = np.fft.fftfreq(mesh_shape[0]) * 2 * np.pi
-    return [
+  kd = np.fft.fftfreq(nc) * 2 * np.pi
+  return [
       jax.make_array_from_callback(
-          (mesh_shape[0], 1, 1),
-          sharding=jax.sharding.NamedSharding(sharding.mesh, P('z')),
+          (nc, 1, 1),
+          sharding=NamedSharding(sharding.mesh, P('z')),
           data_callback=lambda x: kd.reshape([-1, 1, 1])[x]),
       jax.make_array_from_callback(
-          (1, mesh_shape[1], 1),
-          sharding=jax.sharding.NamedSharding(sharding.mesh, P(None, 'y')),
+          (1, nc, 1),
+          sharding=NamedSharding(sharding.mesh, P(None, 'y')),
           data_callback=lambda x: kd.reshape([1, -1, 1])[x]),
       kd.reshape([1, 1, -1])
-    ]
+  ]
+
 
 def gravity_kernel(kvec):
-    """ Fourier kernel to compute gravitational forces from a Fourier space density field.
+  """ Fourier kernel to compute gravitational forces from a Fourier space density field.
 
     Parameters
     ----------
     kvec : tuple of float
         Wave vector in Fourier space.
-    
+
     Returns
     -------
     jnp.ndarray
         Gravitational kernel.
     """
-    kx, ky, kz = kvec
-    kk = jnp.sqrt(kx**2 + ky**2 + kz**2)
-    laplace_kernel = jnp.where(kk == 0, 1., 1. / (kx**2 + ky**2 + kz**2))
-    grav_kernel = [laplace_kernel * 1j / 6.0 * (8 * jnp.sin(kx) - jnp.sin(2 * kx)),
-                   laplace_kernel * 1j / 6.0 * (8 * jnp.sin(ky) - jnp.sin(2 * ky)),
-                   laplace_kernel * 1j / 6.0 * (8 * jnp.sin(kz) - jnp.sin(2 * kz))]
-    return grav_kernel
+  kx, ky, kz = kvec
+  kk = kx**2 + ky**2 + kz**2
+  laplace_kernel = jnp.where(kk == 0, 1., 1. / kk)
+  grav_kernel = [
+      laplace_kernel * 1j / 6.0 * (8 * jnp.sin(kx) - jnp.sin(2 * kx)),
+      laplace_kernel * 1j / 6.0 * (8 * jnp.sin(ky) - jnp.sin(2 * ky)),
+      laplace_kernel * 1j / 6.0 * (8 * jnp.sin(kz) - jnp.sin(2 * kz))
+  ]
+  return grav_kernel
 
-def gaussian_field_and_forces(mesh_shape, box_size, power_spectrum, seed, sharding):
-    """
+
+def gaussian_field_and_forces(key, nc, box_size, power_spectrum, sharding):
+  """
     Generate a Gaussian field with a given power spectrum, along with gravitational forces.
-    
+
     Parameters
     ----------
-    mesh_shape : tuple of int
-        Shape of the mesh.
+    key : int
+        key for the random number generator.
+    nc : int
+        Number of cells in the mesh.
     box_size : float
         Size of the box.
     power_spectrum : callable
         Power spectrum function.
-    seed : int
-        Seed for the random number generator.
     sharding : Any
         Sharding strategy for the array.
-    
+
     Returns
     -------
     delta, forces : tuple of jnp.ndarray
         The generated Gaussian field and the gravitational forces.
     """
-    local_mesh_shape = _global_to_local_size(mesh_shape, sharding)
+  mesh_shape = (nc,) * 3
+  local_mesh_shape = _global_to_local_size(mesh_shape, sharding)
 
-    # Create a distributed field drawn from a Gaussian distribution in real space
-    delta = jax.make_array_from_single_device_arrays(shape=mesh_shape,
-                                                     sharding=sharding,
-                                                     arrays=[jax.random.normal(seed, local_mesh_shape, dtype='float32')])
+  # Create a distributed field drawn from a Gaussian distribution in real space
+  delta = jax.make_array_from_single_device_arrays(
+      shape=mesh_shape,
+      sharding=sharding,
+      arrays=[jax.random.normal(key, local_mesh_shape, dtype='float32')])
 
-    # Compute the Fourier transform of the field
-    delta_k = jaxdecomp.fft.pfft3d(delta.astype(jnp.complex64))
+  # Compute the Fourier transform of the field
+  delta_k = jaxdecomp.fft.pfft3d(delta.astype(jnp.complex64))
 
-    # Compute the Fourier wavenumbers of the field
-    kx, ky, kz = fttk(mesh_shape, sharding)
-    kk = jnp.sqrt((kx / box_size * mesh_shape[0])**2 +
-                  (ky / box_size * mesh_shape[1])**2 +
-                  (kz / box_size * mesh_shape[2])**2)
-    
-    # Apply power spectrum to Fourier modes
-    delta_k *= power_spectrum(kk)**0.5 * jnp.prod(mesh_shape) / jnp.prod(box_size)
-    
-    # Compute inverse Fourier transform to recover the initial conditions in real space
-    delta = jaxdecomp.fft.pifft3d(delta_k).real
+  # Compute the Fourier wavenumbers of the field
+  kx, ky, kz = fttk(nc, sharding)
+  kk = jnp.sqrt(kx**2 + ky**2 + kz**2) * (nc / box_size)**3
 
-    # Compute gravitational forces associated with this field
-    grav_kernel = gravity_kernel([kx, ky, kz])
-    forces_k = [g * delta_k for g in grav_kernel]
+  # Apply power spectrum to Fourier modes
+  delta_k *= (power_spectrum(kk) * (nc / box_size)**3)**0.5
 
-    # Retrieve the forces in real space by inverse Fourier transforming
-    forces = jnp.stack([jaxdecomp.fft.pifft3d(f).real for f in forces_k], axis=-1)
+  # Compute inverse Fourier transform to recover the initial conditions in real space
+  delta = jaxdecomp.fft.pifft3d(delta_k).real
 
-    return delta, forces
+  # Compute gravitational forces associated with this field
+  grav_kernel = gravity_kernel([kx, ky, kz])
+  forces_k = [g * delta_k for g in grav_kernel]
+
+  # Retrieve the forces in real space by inverse Fourier transforming
+  forces = jnp.stack([jaxdecomp.fft.pifft3d(f).real for f in forces_k], axis=-1)
+
+  return delta, forces
 
 
-def cic_paint(displacement, halo_size=32):
-    original_shape = displacement.shape
-    
-    mesh = jnp.zeros(original_shape[:-1], dtype='float32')
-    
-    # Padding the output array along the two first dimensions
-    mesh = jnp.pad(mesh, [[halo_size, halo_size], [halo_size, halo_size], [0, 0]])
+def cic_paint(displacement, sharding, halo_size):
+  """ Paints particles on a mesh using Cloud-In-Cell interpolation.
 
-    a,b,c = jnp.meshgrid(jnp.arange(local_mesh_shape[0]),
-                         jnp.arange(local_mesh_shape[1]), 
-                         jnp.arange(local_mesh_shape[2]))
-    # adding an offset of size halo size
-    pmid = jnp.stack([b+halo_size,a+halo_size,c], axis=-1)
-    pmid = pmid.reshape([-1,3])
-    
-    painted_field = scatter(pmid, displacement.reshape([-1,3]), mesh)
-
-    # Perform halo exchange to get the correct values at the boundaries
-    painted_field = jaxdecomp.halo_exchange(field, 
-                                    halo_extents=(halo_size//2, halo_size//2, 0), 
-                                    halo_periods=(True, True, True), reduce_halo=False)
-    
-    # unpadding the output array
-    field = unpad(painted_field)
-    return field
-
-def simulation_fn(cosmology, mesh_shape, box_size, seed, a, sharding):
-    """
-    Run a simulation to generate initial conditions and density field using LPT.
-    
     Parameters
     ----------
-    mesh_shape : tuple of int
-        Shape of the mesh.
-    box_size : float
-        Size of the box.
-    power_spectrum : callable
-        Power spectrum function.
-    seed : int
-        Seed for the random number generator.
-    a : float
-        Scale factor.
+    displacement : jnp.ndarray
+        Displacement field of particles.
     sharding : Any
         Sharding strategy for the array.
-    
+    halo_size : int
+        Halo size for painting.
+
+    Returns
+    -------
+    jnp.ndarray
+        Density field.
+  """
+  local_mesh_shape = _global_to_local_size(displacement.shape, sharding)
+
+  @partial(
+      shard_map,
+      mesh=sharding.mesh,
+      in_specs=(P('z', 'y'),),
+      out_specs=P('z', 'y'))
+  def cic_op(disp):
+    """ CiC operation on each local slice of the mesh."""
+    # Create a mesh to paint the particles on for the local slice
+    mesh = jnp.zeros(disp.shape[:-1], dtype='float32')
+
+    # Padding the output array along the two first dimensions
+    mesh = jnp.pad(mesh,
+                   [[halo_size, halo_size], [halo_size, halo_size], [0, 0]])
+
+    a, b, c = jnp.meshgrid(
+        jnp.arange(local_mesh_shape[0]), jnp.arange(local_mesh_shape[1]),
+        jnp.arange(local_mesh_shape[2]))
+
+    # adding an offset of size halo size
+    pmid = jnp.stack([b + halo_size, a + halo_size, c], axis=-1)
+    return scatter(pmid.reshape([-1, 3]), disp.reshape([-1, 3]), mesh)
+
+  # Performs painting on padded mesh
+  field = cic_op(displacement)
+
+  # Run halo exchange to get the correct values at the boundaries
+  field = jaxdecomp.halo_exchange(
+      field,
+      halo_extents=(halo_size // 2, halo_size // 2, 0),
+      halo_periods=(True, True, True))
+
+  @partial(
+      shard_map,
+      mesh=sharding.mesh,
+      in_specs=(P('z', 'y'),),
+      out_specs=P('z', 'y'))
+  def unpad(x):
+    """ Unpad the output array. """
+    x = x.at[halo_size:halo_size + halo_size // 2].add(x[:halo_size // 2])
+    x = x.at[-(halo_size + halo_size // 2):-halo_size].add(x[-halo_size // 2:])
+    x = x.at[:, halo_size:halo_size + halo_size // 2].add(x[:, :halo_size // 2])
+    x = x.at[:,
+             -(halo_size + halo_size // 2):-halo_size].add(x[:,
+                                                             -halo_size // 2:])
+    return x[halo_size:-halo_size, halo_size:-halo_size, :]
+
+  # Unpad the output array
+  field = unpad(field)
+  return field
+
+
+def simulation_fn(key, nc, box_size, sharding, halo_size, a=1.0):
+  """
+    Run a simulation to generate initial conditions and density field using LPT.
+
+    Parameters
+    ----------
+    key : list of int
+        Jax random key for the random number generator.
+    nc : int
+        Size of the mesh grid.
+    box_size : float
+        Size of the box.
+    sharding : Any
+        Sharding strategy for the simulation.
+    halo_size: int
+        Halo size for painting.
+    a : float
+        Scale factor of final field.
+
     Returns
     -------
     initial_conditions, field : tuple of jnp.ndarray
         Initial conditions and the density field.
-    """
-    # Define the power spectrum
-    power_spectrum = lambda k: jc.power.linear_matter_power(cosmology, k)
+  """
+  # Build a default cosmology
+  cosmology = jc.Planck15()
 
-    # Generate a Gaussian field and gravitational forces from a power spectrum
-    intial_conditions, initial_forces = gaussian_field_and_forces(mesh_shape, box_size, power_spectrum, seed, sharding)
+  # Create a small function to generate the linear matter power spectrum at arbitrary k
+  k = jnp.logspace(-4, 1, 128)
+  pk = jc.power.linear_matter_power(cosmology, k)
+  pk_fn = jax.jit(lambda x: jc.scipy.interpolate.interp(x.reshape([-1]), k, pk).
+                  reshape(x.shape))
 
-    # Compute the LPT displacement of that particles initialy placed on a regular grid
-    # would experience at scale factor a, by simple Zeldovich approximation
-    initial_displacement = jc.background.growth_factor(cosmology, a) * initial_forces
-    
-    # Paints the displaced particles on a mesh to obtain the density field
-    final_field = cic_paint(initial_displacement, halo_size=32)
+  # Generate a Gaussian field and gravitational forces from a power spectrum
+  intial_conditions, initial_forces = gaussian_field_and_forces(
+      key=key,
+      nc=nc,
+      box_size=box_size,
+      power_spectrum=pk_fn,
+      sharding=sharding)
 
-    return intial_conditions, final_field
+  # Compute the LPT displacement of that particles initialy placed on a regular grid
+  # would experience at scale factor a, by simple Zeldovich approximation
+  initial_displacement = jc.background.growth_factor(
+      cosmology, jnp.atleast_1d(a)) * initial_forces
 
-if __name__ == '__main__':
+  # Paints the displaced particles on a mesh to obtain the density field
+  final_field = cic_paint(initial_displacement, sharding, halo_size)
 
-  jax.config.update('jax_enable_x64', False)
+  return intial_conditions, final_field
 
-  parser = argparse.ArgumentParser()
 
-  parser.add_argument('-s', '--size', type=int, default=64)
-  parser.add_argument('-p', '--pdims', type=str, default='1x1')
-  parser.add_argument('-b', '--box_size', type=int, default=200)
-  parser.add_argument('-hs', '--halo_size', type=int, default=32)
-  parser.add_argument('-o', '--output', type=str, default='out')
-
-  args = parser.parse_args()
-
+def main(args):
   print(f"Running with arguments {args}")
 
-  # *********************************
-  # Setup
-  # *********************************
+  # Setting up distributed jax
+  jax.distributed.initialize()
+  rank = jax.process_index()
+  size = jax.process_count()
+
+  # Setting up distributed random numbers
   master_key = jax.random.PRNGKey(42)
   key = jax.random.split(master_key, size)[rank]
-  # Read parameters
+
+  # Create computing mesh and sharding information
   pdims = tuple(map(int, args.pdims.split('x')))
-  mesh_shape = (args.size, args.size, args.size)
-  box_size = [float(args.box_size), float(args.box_size), float(args.box_size)]
-  halo_size = args.halo_size
-
-  output_dir = args.output
-  # Create output directory recursively
-  os.makedirs(output_dir, exist_ok=True)
-
-  # Create computing mesh
   devices = mesh_utils.create_device_mesh(pdims)
   mesh = Mesh(devices, axis_names=('y', 'z'))
   sharding = jax.sharding.NamedSharding(mesh, P('z', 'y'))
-  replicate = jax.sharding.NamedSharding(mesh, P())
-
-  print(f"Saving on folder {output_dir}")
-  print(f"Created initial field {z.shape} and sharding {z.sharding}")
-  print(f"Created painting mesh with shape {painting_mesh.shape}")
-  print(f"And sharding  {painting_mesh.sharding}")
-  print(f"Created positions {pos.shape} and sharding {pos.sharding}")
-  print("Corrected positions for rank {rank}  --> ")
-  print(
-      f"pos shape {pos.shape} pos sharding = {pos.sharding} shape of local {pos.addressable_data(0).shape}"
-  )
 
   with mesh:
-    jit_start = time.perf_counter()
-    initial_conds, field = intial_conditions(z, kvec, pos, painting_mesh, a=1.)
-    field.block_until_ready()
-    jit_end = time.perf_counter()
+    # Run the simulation on the compute mesh
+    initial_conds, final_field = simulation_fn(
+        key=key,
+        nc=args.nc,
+        box_size=args.box_size,
+        sharding=sharding,
+        halo_size=args.halo_size)
 
-    print(f"JIT done in {jit_end - jit_start}")
-
-    start = time.perf_counter()
-    initial_conds, field = forward_fn(z, kvec, pos, painting_mesh, a=1.)
-    field.block_until_ready()
-    end = time.perf_counter()
-
-    print(f"Execution done in {end - start}")
-
-  with open(f"{output_dir}/log_{rank}.log", 'w') as log_file:
-    log_file.write(f"JIT time: {jit_end - jit_start}\n")
-    log_file.write(f"Execution time: {end - start}\n")
-
-  # Saving results
+  # Create output directory to save the results
+  output_dir = args.output
+  os.makedirs(output_dir, exist_ok=True)
   np.save(f'{output_dir}/initial_conditions_{rank}.npy',
           initial_conds.addressable_data(0))
-  np.save(f'{output_dir}/field_{rank}.npy', field.addressable_data(0))
-
+  np.save(f'{output_dir}/field_{rank}.npy', final_field.addressable_data(0))
   print(f"Finished saved to {output_dir}")
 
-jax.distributed.shutdown()
+  # Closing distributed jax
+  jax.distributed.shutdown()
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser("LPT N-body simulation with JAX.")
+  parser.add_argument(
+      '--pdims', type=str, default='1x1', help="Processor grid dimensions")
+  parser.add_argument(
+      '--nc', type=int, default=256, help="Number of cells in the mesh")
+  parser.add_argument(
+      '--box_size',
+      type=float,
+      default=256.,
+      help="Size of the simulation box in Mpc/h")
+  parser.add_argument(
+      '--halo_size', type=int, default=32, help="Halo size for painting")
+  parser.add_argument('--output', type=str, default='out')
+  args = parser.parse_args()
+
+  main(args)
--- examples/scatter.py
+++ examples/scatter.py
@@ -34,122 +34,127 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 import jax
-import jax.numpy as jnp
 import jax.lax as lax
+import jax.numpy as jnp
 from jax.lax import scan
 
+
 def _chunk_split(ptcl_num, chunk_size, *arrays):
-    """Split and reshape particle arrays into chunks and remainders, with the remainders
+  """Split and reshape particle arrays into chunks and remainders, with the remainders
     preceding the chunks. 0D ones are duplicated as full arrays in the chunks."""
-    chunk_size = ptcl_num if chunk_size is None else min(chunk_size, ptcl_num)
-    remainder_size = ptcl_num % chunk_size
-    chunk_num = ptcl_num // chunk_size
+  chunk_size = ptcl_num if chunk_size is None else min(chunk_size, ptcl_num)
+  remainder_size = ptcl_num % chunk_size
+  chunk_num = ptcl_num // chunk_size
 
-    remainder = None
-    chunks = arrays
-    if remainder_size:
-        remainder = [x[:remainder_size] if x.ndim != 0 else x for x in arrays]
-        chunks = [x[remainder_size:] if x.ndim != 0 else x for x in arrays]
+  remainder = None
+  chunks = arrays
+  if remainder_size:
+    remainder = [x[:remainder_size] if x.ndim != 0 else x for x in arrays]
+    chunks = [x[remainder_size:] if x.ndim != 0 else x for x in arrays]
 
-    # `scan` triggers errors in scatter and gather without the `full`
-    chunks = [x.reshape(chunk_num, chunk_size, *x.shape[1:]) if x.ndim != 0
-              else jnp.full(chunk_num, x) for x in chunks]
+  # `scan` triggers errors in scatter and gather without the `full`
+  chunks = [
+      x.reshape(chunk_num, chunk_size, *x.shape[1:])
+      if x.ndim != 0 else jnp.full(chunk_num, x) for x in chunks
+  ]
 
-    return remainder, chunks
+  return remainder, chunks
+
 
 def enmesh(i1, d1, a1, s1, b12, a2, s2):
-    """Multilinear enmeshing."""
-    i1 = jnp.asarray(i1)
-    d1 = jnp.asarray(d1)
-    a1 = jnp.float64(a1) if a2 is not None else jnp.array(a1, dtype=d1.dtype)
+  """Multilinear enmeshing."""
+  i1 = jnp.asarray(i1)
+  d1 = jnp.asarray(d1)
+  a1 = jnp.float64(a1) if a2 is not None else jnp.array(a1, dtype=d1.dtype)
+  if s1 is not None:
+    s1 = jnp.array(s1, dtype=i1.dtype)
+  b12 = jnp.float64(b12)
+  if a2 is not None:
+    a2 = jnp.float64(a2)
+  if s2 is not None:
+    s2 = jnp.array(s2, dtype=i1.dtype)
+
+  dim = i1.shape[1]
+  neighbors = (jnp.arange(2**dim, dtype=i1.dtype)[:, jnp.newaxis] >> jnp.arange(
+      dim, dtype=i1.dtype)) & 1
+
+  if a2 is not None:
+    P = i1 * a1 + d1 - b12
+    P = P[:, jnp.newaxis]  # insert neighbor axis
+    i2 = P + neighbors * a2  # multilinear
+
     if s1 is not None:
-        s1 = jnp.array(s1, dtype=i1.dtype)
-    b12 = jnp.float64(b12)
-    if a2 is not None:
-        a2 = jnp.float64(a2)
-    if s2 is not None:
-        s2 = jnp.array(s2, dtype=i1.dtype)
+      L = s1 * a1
+      i2 %= L
 
-    dim = i1.shape[1]
-    neighbors = (jnp.arange(2**dim, dtype=i1.dtype)[:, jnp.newaxis]
-                 >> jnp.arange(dim, dtype=i1.dtype)
-                ) & 1
+    i2 //= a2
+    d2 = P - i2 * a2
 
-    if a2 is not None:
-        P = i1 * a1 + d1 - b12
-        P = P[:, jnp.newaxis]  # insert neighbor axis
-        i2 = P + neighbors * a2  # multilinear
+    if s1 is not None:
+      d2 -= jnp.rint(d2 / L) * L  # also abs(d2) < a2 is expected
 
-        if s1 is not None:
-            L = s1 * a1
-            i2 %= L
+    i2 = i2.astype(i1.dtype)
+    d2 = d2.astype(d1.dtype)
+    a2 = a2.astype(d1.dtype)
 
-        i2 //= a2
-        d2 = P - i2 * a2
+    d2 /= a2
+  else:
+    i12, d12 = jnp.divmod(b12, a1)
+    i1 -= i12.astype(i1.dtype)
+    d1 -= d12.astype(d1.dtype)
 
-        if s1 is not None:
-            d2 -= jnp.rint(d2 / L) * L  # also abs(d2) < a2 is expected
+    # insert neighbor axis
+    i1 = i1[:, jnp.newaxis]
+    d1 = d1[:, jnp.newaxis]
 
-        i2 = i2.astype(i1.dtype)
-        d2 = d2.astype(d1.dtype)
-        a2 = a2.astype(d1.dtype)
+    # multilinear
+    d1 /= a1
+    i2 = jnp.floor(d1).astype(i1.dtype)
+    i2 += neighbors
+    d2 = d1 - i2
+    i2 += i1
 
-        d2 /= a2
-    else:
-        i12, d12 = jnp.divmod(b12, a1)
-        i1 -= i12.astype(i1.dtype)
-        d1 -= d12.astype(d1.dtype)
+    if s1 is not None:
+      i2 %= s1
 
-        # insert neighbor axis
-        i1 = i1[:, jnp.newaxis]
-        d1 = d1[:, jnp.newaxis]
+  f2 = 1 - jnp.abs(d2)
 
-        # multilinear
-        d1 /= a1
-        i2 = jnp.floor(d1).astype(i1.dtype)
-        i2 += neighbors
-        d2 = d1 - i2
-        i2 += i1
+  if s1 is None and s2 is not None:  # all i2 >= 0 if s1 is not None
+    i2 = jnp.where(i2 < 0, s2, i2)
 
-        if s1 is not None:
-            i2 %= s1
+  f2 = f2.prod(axis=-1)
 
-    f2 = 1 - jnp.abs(d2)
+  return i2, f2
 
-    if s1 is None and s2 is not None:  # all i2 >= 0 if s1 is not None
-        i2 = jnp.where(i2 < 0, s2, i2)
-
-    f2 = f2.prod(axis=-1)
-
-    return i2, f2
 
 def scatter(pmid, disp, mesh, chunk_size=2**24, val=1., offset=0, cell_size=1.):
-    ptcl_num, spatial_ndim = pmid.shape
-    val = jnp.asarray(val)
-    mesh = jnp.asarray(mesh)
+  ptcl_num, spatial_ndim = pmid.shape
+  val = jnp.asarray(val)
+  mesh = jnp.asarray(mesh)
 
-    remainder, chunks = _chunk_split(ptcl_num, chunk_size, pmid, disp, val)
+  remainder, chunks = _chunk_split(ptcl_num, chunk_size, pmid, disp, val)
 
-    carry = mesh, offset, cell_size
-    if remainder is not None:
-        carry = _scatter_chunk(carry, remainder)[0]
-    carry = scan(_scatter_chunk, carry, chunks)[0]
-    mesh = carry[0]
+  carry = mesh, offset, cell_size
+  if remainder is not None:
+    carry = _scatter_chunk(carry, remainder)[0]
+  carry = scan(_scatter_chunk, carry, chunks)[0]
+  mesh = carry[0]
 
-    return mesh
+  return mesh
+
 
 def _scatter_chunk(carry, chunk):
-    mesh, offset, cell_size = carry
-    pmid, disp, val = chunk
-    spatial_ndim = pmid.shape[1]
-    spatial_shape = mesh.shape
+  mesh, offset, cell_size = carry
+  pmid, disp, val = chunk
+  spatial_ndim = pmid.shape[1]
+  spatial_shape = mesh.shape
 
-    # multilinear mesh indices and fractions
-    ind, frac = enmesh(pmid, disp, cell_size, spatial_shape,
-                       offset, cell_size, spatial_shape)
-    # scatter
-    ind = tuple(ind[..., i] for i in range(spatial_ndim))
-    mesh = mesh.at[ind].add(val * frac)
+  # multilinear mesh indices and fractions
+  ind, frac = enmesh(pmid, disp, cell_size, spatial_shape, offset, cell_size,
+                     spatial_shape)
+  # scatter
+  ind = tuple(ind[..., i] for i in range(spatial_ndim))
+  mesh = mesh.at[ind].add(val * frac)
 
-    carry = mesh, offset, cell_size
-    return carry, None
+  carry = mesh, offset, cell_size
+  return carry, None
