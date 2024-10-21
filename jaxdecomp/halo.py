6ce7cf0865d0323525608028bf6549d00bee167e
Wassim KABALAN <wastondev@gmail.com>
2024-10-20 23:39:55 -0400

    revert halo to tuple pdims

--- jaxdecomp/halo.py
+++ jaxdecomp/halo.py
@@ -5,15 +5,15 @@ from jaxtyping import Array
 from jaxdecomp._src.cudecomp.halo import \
     halo_exchange as _cudecomp_halo_exchange
 from jaxdecomp._src.jax.halo import halo_exchange as _jax_halo_exchange
-
+from jaxdecomp._src.jax.halo import HaloExtentType , Periodicity
 
 def halo_exchange(x: Array,
-                  halo_extent: int,
-                  periodic: bool,
+                  halo_extents: HaloExtentType,
+                  halo_periods: Periodicity,
                   backend: str = "jax") -> Array:
   if backend.lower() == "jax":
-    return _jax_halo_exchange(x, halo_extent, periodic)
+    return _jax_halo_exchange(x, halo_extents, halo_periods)
   elif backend.lower() == "cudecomp":
-    return _cudecomp_halo_exchange(x, halo_extent, periodic)
+    return _cudecomp_halo_exchange(x, halo_extents, halo_periods)
   else:
     raise ValueError(f"Invalid backend: {backend}")
