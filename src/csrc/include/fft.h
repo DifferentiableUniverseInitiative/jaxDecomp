7900de56ce7f536cb08763063bbba243f4ff4c4b
Wassim KABALAN <wastondev@gmail.com>
2024-10-24 17:13:41 +0200

    fix cudecomp build

--- src/csrc/include/fft.h
+++ src/csrc/include/fft.h
@@ -24,7 +24,7 @@ static cudecompDataType_t get_cudecomp_datatype(cuda::std::complex<float>) { ret
 static cudecompDataType_t get_cudecomp_datatype(cuda::std::complex<double>) { return CUDECOMP_DOUBLE_COMPLEX; }
 namespace jaxdecomp {
 
-enum Decomposition { slab_XY, slab_YZ, pencil, unknown };
+enum Decomposition { slab_XY = 0, slab_YZ = 1, pencil = 2, no_decomp = 3 };
 
 static Decomposition GetDecomposition(const int pdims[2]) {
   if (pdims[0] == 1 && pdims[1] > 1) {
@@ -34,9 +34,7 @@ static Decomposition GetDecomposition(const int pdims[2]) {
   } else if (pdims[0] > 1 && pdims[1] > 1) {
     return Decomposition::pencil;
   }
-  // Return pencils on one devices for testing
-  return Decomposition::pencil;
-  // return Decomposition::unknown;
+  return Decomposition::no_decomp;
 }
 
 // fftDescriptor hash should be triavially computable
@@ -44,6 +42,7 @@ static Decomposition GetDecomposition(const int pdims[2]) {
 class fftDescriptor {
 public:
   bool adjoint = false;
+  bool contiguous = true;
   bool forward = true; ///< forward or backward pass
   // fft_is_forward_pass and forwad are used for the Execution but not for the
   // hash This way IFFT and FFT have the same plans when operating with the same
@@ -51,7 +50,7 @@ public:
   int32_t gdims[3]; ///< dimensions of global data grid
   // Decomposition type is used in order to allow reusing plans
   // from the XY and XZ forward pass for ZY and YZ backward pass respectively
-  Decomposition decomposition = Decomposition::unknown; ///< decomposition type
+  Decomposition decomposition = Decomposition::no_decomp; ///< decomposition type
   bool double_precision = false;
   cudecompGridDescConfig_t config; // Descriptor for the grid
 
@@ -63,7 +62,7 @@ public:
   // created in the bottom of the file)
   bool operator==(const fftDescriptor& other) const {
     if (double_precision != other.double_precision || gdims[0] != other.gdims[0] || gdims[1] != other.gdims[1] ||
-        gdims[2] != other.gdims[2] || decomposition != other.decomposition) {
+        gdims[2] != other.gdims[2] || decomposition != other.decomposition || contiguous != other.contiguous) {
       return false;
     }
     return true;
@@ -74,14 +73,15 @@ public:
   // this is used for subsequent ffts to find the Executor that was already
   // defined
   fftDescriptor(cudecompGridDescConfig_t& config, const bool& double_precision, const bool& iForward,
-                const bool& iAdjoint)
-      : double_precision(double_precision), config(config) {
+                const bool& iAdjoint, const bool& iContiguous, const Decomposition& iDecomposition)
+      : double_precision(double_precision), config(config), forward(iForward), contiguous(iContiguous),
+        adjoint(iAdjoint), decomposition(iDecomposition) {
     gdims[0] = config.gdims[0];
     gdims[1] = config.gdims[1];
     gdims[2] = config.gdims[2];
-    forward = iForward;
-    adjoint = iAdjoint;
-    decomposition = GetDecomposition(config.pdims);
+    this->config.transpose_axis_contiguous[0] = iContiguous;
+    this->config.transpose_axis_contiguous[1] = iContiguous;
+    this->config.transpose_axis_contiguous[2] = iContiguous;
   }
 };
 
@@ -96,8 +96,7 @@ public:
   FourierExecutor() : m_Tracer("JAXDECOMP") {}
   ~FourierExecutor();
 
-  HRESULT Initialize(cudecompHandle_t handle, cudecompGridDescConfig_t config, size_t& work_size,
-                     fftDescriptor& fft_descriptor);
+  HRESULT Initialize(cudecompHandle_t handle, size_t& work_size, fftDescriptor& fft_descriptor);
 
   HRESULT forward(cudecompHandle_t handle, fftDescriptor desc, cudaStream_t stream, void** buffers);
 
@@ -114,6 +113,9 @@ private:
 
   cudecompGridDesc_t m_GridConfig;
   cudecompGridDescConfig_t m_GridDescConfig;
+  cudecompPencilInfo_t m_XPencilInfo;
+  cudecompPencilInfo_t m_YPencilInfo;
+  cudecompPencilInfo_t m_ZPencilInfo;
   // For the sake of expressive code, plans have the name of their corresponding
   // goal Instead of reusing pencils plans for slabs, or even ZY to YZ we store
   // properly named plans
@@ -122,25 +124,20 @@ private:
   cufftHandle m_Plan_c2c_x;
   cufftHandle m_Plan_c2c_y;
   cufftHandle m_Plan_c2c_z;
-  // For Slabs XY
-  cufftHandle m_Plan_c2c_xy;
-  // For Slabs XZ
+  // For Slabs XY  FFT (Y) FFT(XZ) but JAX redifines the axes to YZX as X pencil for cudecomp
+  // so in the end it is FFT(X) FFT(YZ)
+  // For Slabs XZ FFT (X) FFT(YZ)
   cufftHandle m_Plan_c2c_yz;
+  cufftHandle m_Plan_c2c_xy;
   // work size
   int64_t m_WorkSize;
 
   // Internal functions
-  HRESULT InitializePencils(cudecompGridDescConfig_t& iGridConfig, cudecompPencilInfo_t& x_pencil_info,
-                            cudecompPencilInfo_t& y_pencil_info, cudecompPencilInfo_t& z_pencil_info,
-                            int64_t& work_size, const bool& is_contiguous);
+  HRESULT InitializePencils(int64_t& work_size, fftDescriptor& fft_descriptor);
 
-  HRESULT InitializeSlabXY(cudecompGridDescConfig_t& iGridConfig, cudecompPencilInfo_t& x_pencil_info,
-                           cudecompPencilInfo_t& y_pencil_info, cudecompPencilInfo_t& z_pencil_info, int64_t& work_size,
-                           const bool& is_contiguous);
+  HRESULT InitializeSlabXY(int64_t& work_size, fftDescriptor& fft_descriptor);
 
-  HRESULT InitializeSlabYZ(cudecompGridDescConfig_t& iGridConfig, cudecompPencilInfo_t& x_pencil_info,
-                           cudecompPencilInfo_t& y_pencil_info, cudecompPencilInfo_t& z_pencil_info, int64_t& work_size,
-                           const bool& is_contiguous);
+  HRESULT InitializeSlabYZ(int64_t& work_size, fftDescriptor& fft_descriptor);
 
   HRESULT forwardXY(cudecompHandle_t handle, fftDescriptor desc, cudaStream_t stream, complex_t* input,
                     complex_t* output, complex_t* work_buffer);
@@ -161,9 +158,6 @@ private:
                          complex_t* output, complex_t* work_buffer);
 
   HRESULT clearPlans();
-
-  // DEBUG ONLY ... I WARN YOU
-  void inspect_device_array(complex_t* data, int size, cudaStream_t stream);
 };
 
 } // namespace jaxdecomp
@@ -174,11 +168,9 @@ template <> struct hash<jaxdecomp::fftDescriptor> {
     // Only hash The double precision and the gdims and pdims
     // If adjoint is changed then the plan should be the same
     // adjoint is to be used to execute the backward plan
-    static const size_t xy_hash = std::hash<int>()(jaxdecomp::Decomposition::slab_XY);
-
     size_t hash = std::hash<double>()(descriptor.double_precision) ^ std::hash<int>()(descriptor.gdims[0]) ^
                   std::hash<int>()(descriptor.gdims[1]) ^ std::hash<int>()(descriptor.gdims[2]) ^
-                  std::hash<int>()(descriptor.decomposition);
+                  std::hash<int>()(descriptor.decomposition) ^ std::hash<bool>()(descriptor.contiguous);
     return hash;
   }
 };
