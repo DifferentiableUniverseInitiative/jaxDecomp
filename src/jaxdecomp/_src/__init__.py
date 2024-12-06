813153ecb6a8cb82cccea5ceea69006aa2cbefd8
Wassim Kabalan <wastondev@gmail.com>
2024-12-06 19:34:54 +0100

    bump version

--- src/jaxdecomp/_src/__init__.py
+++ src/jaxdecomp/_src/__init__.py
@@ -8,20 +8,15 @@ get_autotuned_config = _jaxdecomp.get_autotuned_config
 make_config = _jaxdecomp.GridConfig
 
 # Loading the comm configuration flags at the top level
+from jaxdecomplib._jaxdecomp import HaloCommBackend  # yapf: disable
 from jaxdecomplib._jaxdecomp import TransposeCommBackend  # yapf: disable
-from jaxdecomplib._jaxdecomp import (HALO_COMM_MPI, HALO_COMM_MPI_BLOCKING,
-                                     HALO_COMM_NCCL, HALO_COMM_NVSHMEM,
-                                     HALO_COMM_NVSHMEM_BLOCKING, NO_DECOMP,
-                                     PENCILS, SLAB_XY, SLAB_YZ,
-                                     TRANSPOSE_COMM_MPI_A2A,
-                                     TRANSPOSE_COMM_MPI_P2P,
-                                     TRANSPOSE_COMM_MPI_P2P_PL,
-                                     TRANSPOSE_COMM_NCCL,
-                                     TRANSPOSE_COMM_NCCL_PL,
-                                     TRANSPOSE_COMM_NVSHMEM,
-                                     TRANSPOSE_COMM_NVSHMEM_PL, TRANSPOSE_XY,
-                                     TRANSPOSE_YX, TRANSPOSE_YZ, TRANSPOSE_ZY,
-                                     HaloCommBackend)
+from jaxdecomplib._jaxdecomp import (  # dummy line to avoid yapf reformatting
+    HALO_COMM_MPI, HALO_COMM_MPI_BLOCKING, HALO_COMM_NCCL, HALO_COMM_NVSHMEM,
+    HALO_COMM_NVSHMEM_BLOCKING, NO_DECOMP, PENCILS, SLAB_XY, SLAB_YZ,
+    TRANSPOSE_COMM_MPI_A2A, TRANSPOSE_COMM_MPI_P2P, TRANSPOSE_COMM_MPI_P2P_PL,
+    TRANSPOSE_COMM_NCCL, TRANSPOSE_COMM_NCCL_PL, TRANSPOSE_COMM_NVSHMEM,
+    TRANSPOSE_COMM_NVSHMEM_PL, TRANSPOSE_XY, TRANSPOSE_YX, TRANSPOSE_YZ,
+    TRANSPOSE_ZY)
 
 # Registering ops for XLA
 for name, fn in _jaxdecomp.registrations().items():
